50c88e130a923b452ea24080901057f429d62c57
Silas Bartel <silas.a.bartel@gmail.com>
2021-06-29 15:04:19 +0200

    Updated cloth sets. More entries overall, variants and categories.

--- suitable-cloth-recommender/src/logic/cloth_sets.mjs
+++ suitable-cloth-recommender/src/logic/cloth_sets.mjs
@@ -1,26 +1,86 @@
 export let cloth_sets = [
   {
     description: 'Walking shorts, short-sleeved shirt',
+    category: ['sport', 'casual'],
     insulation: 0.36,
     filename: 'shortshort.png'
   },
   {
+    description: 'Walking shorts, short-sleeved shirt plus long-sleeve sweater',
+    category: ['sport', 'casual'],
+    insulation: 0.36,
+    variants: [
+      {
+        description: 'sweater on',
+        insulation: 0.61
+      }
+    ],
+  },
+  {
+    description: 'Walking shorts, long-sleeved shirt',
+    category: ['sport', 'casual'],
+    insulation: 0.40,
+  },
+  {
     description: 'Trousers, short-sleeved shirt',
+    category: ['sport', 'casual'],
     insulation: 0.57,
     filename: 'shortlong.jpg'
   },
   {
+    description: 'Trousers, short-sleeved shirt plus sweater',
+    category: ['sport', 'casual'],
+    insulation: 0.57,
+    variants: [
+      {
+        description: 'sweater on',
+        insulation: 0.82
+      }
+    ],
+  },
+  {
+    description: 'Trousers, short-sleeved shirt plus jacket',
+    category: ['sport', 'casual'],
+    insulation: 0.57,
+    variants: [
+      {
+        description: 'jacket on',
+        insulation: 0.92
+      }
+    ],
+  },
+  {
     description: 'Trousers, long-sleeved shirt',
+    category: ['sport', 'casual'],
     insulation: 0.61,
     filename: 'longlong.jpg'
   },
   {
-    description: 'Trousers, long-sleeved shirt, plus suit jacket',
-    insulation: 0.96,
+    description: 'Trousers, long-sleeved shirt plus sweater',
+    category: ['sport', 'casual'],
+    insulation: 0.61,
+    variants: [
+      {
+        description: 'sweater on',
+        insulation: 0.86
+      }
+    ],
+  },
+  {
+    description: 'Trousers, long-sleeved shirt plus jacket',
+    category: ['sport', 'casual'],
+    insulation: 0.61,
+    variants: [
+      {
+        description: 'jacket on',
+        insulation: 0.96
+      }
+    ],
     filename: 'jacket.png'
   },
   {
     description: 'Trousers, long-sleeved shirt, long-sleeved sweater, T-shirt',
+    category: ['casual'],
     insulation: 1.01,
     filename: '2000px-Stick_Figure.svg.png'
   },
