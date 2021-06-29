ef807024388c01cff25a59db3447775355340ef4
Silas Bartel <silas.a.bartel@gmail.com>
2021-06-29 14:10:22 +0200

    Added data about activities and their associated thermal heating.

---
+++ suitable-cloth-recommender/src/logic/activities.mjs
@@ -0,0 +1,12 @@
+// name, thermal power
+export let activities = [
+  ['Resting - Reclining', 45],
+  ['Resting - Seated', 60],
+  ['Resting - Standing', 70],
+  ['Walking - Slow', 115],
+  ['Walking - Medium', 150],
+  ['Walking - Fast', 220],
+  ['Sport - Light', 180],
+  ['Sport - Medium', 300],
+  ['Sport - Heavy', 400],
+]
