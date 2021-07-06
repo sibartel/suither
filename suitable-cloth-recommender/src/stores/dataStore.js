189003feebd0a29ca596d86f1757258045a946f9
Silas Bartel <silas.a.bartel@gmail.com>
2021-07-06 20:56:26 +0200

    Made dataStore persistant in localStorage.

--- suitable-cloth-recommender/src/stores/dataStore.js
+++ suitable-cloth-recommender/src/stores/dataStore.js
@@ -1,15 +1,23 @@
 import { writable } from 'svelte/store';
 
-export const dataStore = writable(
-    {
-        self_assessed_sensation_deviation: 0.0,
-        current_cloth_set: null,
-        recommender_settings: {
-            relevant_hours: 5,
-            activity: 60,
-            category: null,
-            ignore_rain: true
-        },
-        reviews: []       
-    }
-) 
+const key = 'data_store'
+
+let stored_data = {
+    self_assessed_sensation_deviation: 0.0,
+    current_cloth_set: null,
+    recommender_settings: {
+        relevant_hours: 5,
+        activity: 60,
+        category: null,
+        ignore_rain: true
+    },
+    reviews: []       
+}
+
+if (typeof localStorage !== 'undefined' && localStorage.getItem(key))
+    stored_data = JSON.parse(localStorage.getItem(key))
+
+export const dataStore = writable(stored_data)
+
+if (typeof localStorage !== 'undefined')
+    dataStore.subscribe(data => localStorage.setItem(key, JSON.stringify(data)))
