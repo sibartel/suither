8920e1fee3092c6d60555c100ac852aafeb31bb0
Silas Bartel <silas.a.bartel@gmail.com>
2021-07-07 18:17:59 +0200

    Small ui changes. Fixed background. More details on about page.

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -2,7 +2,21 @@
 	<title>About</title>
 </svelte:head>
 
-<h4>Authors</h4>
+<h4>Suither - A Cloth Recommender</h4>
 
-<p>Enrico Kögler</p>
-<p>Silas Bartel</p>
+<p>
+	A prototype PWA with the intent to propose weather-suitable outfits
+	to support the users with their daily cloth selection.
+</p>
+<p>
+	This project was developed as part of the <em>Intelligent User Interfaces</em>
+	course @ University of Bayreuth in the SS21:
+	<a target="_blank" rel=external href="https://github.com/sibartel/suitable-cloth-recommender">Github</a>
+</p>
+
+<h5>Authors</h5>
+
+<ul>
+	<li>Enrico Kögler - <a target="_blank" rel=external href="https://github.com/EnricoKoegler">Github</a></li>
+	<li>Silas Bartel - <a target="_blank" rel=external href="https://github.com/sibartel">Github</a></li>
+</ul>
